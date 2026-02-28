984d9f771d8999a2fbaa00c0ffe4d8b1e90b9e6d
LiMingCong <2058615288@qq.com>
2026-02-28 17:15:18 +0800

    菜单显示效果实现

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,10 +1,28 @@
-<script setup>
-
-</script>
-
 <template>
-  <div>侧边栏</div>
+  <el-menu
+      default-active="dashboard"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      router
+  >
+    <el-menu-item v-for="item in router.options.routes[1].children" :key="item.path" :index="item.path">
+      <el-icon><component :is="item.meta.icon"></component></el-icon>
+      <span>{{ item.meta.title }}</span>
+    </el-menu-item>
+  </el-menu>
 </template>
+
+<script setup>
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+const handleOpen = () => {
+
+}
+
+const handleClose = () => {}
+</script>
 
 <style scoped lang="scss">
 
